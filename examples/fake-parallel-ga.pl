e29ed02f1adfbeb7ea606824a577bede494f8b61
jmerelo <jmerelo>
2008-02-18 13:35:45 +0000

    test logging using yaml in examples

--- examples/fake-parallel-ga.pl
+++ examples/fake-parallel-ga.pl
@@ -23,23 +23,31 @@ use lib qw(lib ../lib);
 use Algorithm::Evolutionary::Run;
 
 use POE;
-use YAML qw(Dump);
+use YAML qw(Dump LoadFile);
+use IO::YAML;
+use DateTime;
 
-my $params = shift || die "Usage: $0 params.yaml\n";
+my $spec = shift || die "Usage: $0 params.yaml conf.yaml\n";
+my $params_file = shift || "conf.yaml";
+my $conf = LoadFile( $params_file ) || die "Can't open $params_file: $@\n";
 
-for my $s (1..2) {
+for my $s (1..$conf->{'sessions'}) {
   POE::Session->create(inline_states => { _start => \&start,
-					  generation => \&generation},
+					  generation => \&generation,
+					finish => \&finishing},
 		       args  => [$s]
 		       );
 }
 
-print "Running kernel\n";
 
+my @sigils = ( '<','>' );
 #Time
+my $io = IO::YAML->new($conf->{'ID'}.".yaml", ">");
+$io->print( { DateTime->now => 'Start' });
 $poe_kernel->post( "Population 1", "generation", "Population 2");
 $poe_kernel->run();
-print "Exiting\n";
+$io->print( { DateTime->now => "Exiting" });
+$io->close();
 exit(0);
 
 
@@ -48,7 +56,7 @@ exit(0);
 sub start {
   my ($kernel, $heap, $session ) = @_[KERNEL, HEAP, ARG0];
   $kernel->alias_set("Population $session");
-  my $algorithm =  new Algorithm::Evolutionary::Run $params;
+  my $algorithm =  new Algorithm::Evolutionary::Run $spec;
   $heap->{'algorithm'} = $algorithm;
   $heap->{'counter'} = 0;
 }
@@ -56,27 +64,35 @@ sub start {
 #------------------------------------------------------------#
 
 sub generation {
-  my ($kernel, $heap, $session, $next, $other_best ) = @_[KERNEL, HEAP, SESSION, ARG0, ARG1];
 
+  my ($kernel, $heap, $session, $next, $other_best ) = @_[KERNEL, HEAP, SESSION, ARG0, ARG1];
   my $alias =  $kernel->alias_list($session);
   my $algorithm = $heap->{'algorithm'};
+  my $now =  DateTime->now();
+  my $hash = { $now => {}};
+  
   if ( $other_best ) {
-    print "\tReceiving ", $other_best->asString(), "\n";
+    $hash->{$now}->{'Receiving'} = $other_best;
     pop @{$algorithm->{'_population'}};
     push @{$algorithm->{'_population'}}, $other_best;
   }
   $algorithm->run();
   my $best = $algorithm->results()->{'best'};
-  print $alias, " : ", $best->asString(), "\n";
+  $hash->{$now}->{'best'} = $best;
   if ( ( $best->Fitness() < $algorithm->{'max_fitness'} ) 
-       && ( $heap->{'counter'}++ < 60 ) ) {
+       && ( $heap->{'counter'}++ < $conf->{'max_runs'} ) ) {
     $kernel->post($next, 'generation', $session->ID, $best );
   } else {
-    print "Terminamos : ", Dump( $algorithm->results );
+    $kernel->post($session->ID, 'finish');
+    $kernel->post($next, 'finish');
   }
+  print $io $hash;
 }
 
-
+sub finishing {
+  my $heap   = $_[ HEAP ];
+  $io->print( {DateTime->now() => { Finish => $heap->{'algorithm'}->results }}) ;
+}
 
 =head1 AUTHOR
 
@@ -89,10 +105,10 @@ J. J. Merelo C<jj@merelo.net>
   This file is released under the GPL. See the LICENSE file included in this distribution,
   or go to http://www.fsf.org/licenses/gpl.txt
 
-  CVS Info: $Date: 2008/02/17 18:01:48 $ 
-  $Header: /media/Backup/Repos/opeal/opeal/Algorithm-Evolutionary/examples/fake-parallel-ga.pl,v 1.1 2008/02/17 18:01:48 jmerelo Exp $ 
+  CVS Info: $Date: 2008/02/18 13:35:45 $ 
+  $Header: /media/Backup/Repos/opeal/opeal/Algorithm-Evolutionary/examples/fake-parallel-ga.pl,v 1.2 2008/02/18 13:35:45 jmerelo Exp $ 
   $Author: jmerelo $ 
-  $Revision: 1.1 $
+  $Revision: 1.2 $
   $Name $
 
 =cut
