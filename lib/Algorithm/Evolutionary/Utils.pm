68405bc51a50e239ff651904ba23c3950c2308ae
jmerelo <jmerelo>
2008-04-20 11:03:12 +0000

    Wrap up

--- lib/Algorithm/Evolutionary/Utils.pm
+++ lib/Algorithm/Evolutionary/Utils.pm
@@ -11,7 +11,9 @@ use warnings;
 
   my $this_entropy = entropy( $population );
 
-  
+  #Computes consensus sequence (for binary chromosomes
+  my $this_consensus = consensus( $population); 
+
 =head1 DESCRIPTION
 
 Miscellaneous class that contains functions that might be useful
@@ -28,14 +30,15 @@ package Algorithm::Evolutionary::Utils;
 
 use Exporter;
 our @ISA = qw(Exporter);
-our $VERSION = ( '$Revision: 1.1 $ ' =~ /(\d+\.\d+)/ ) ;
-our @EXPORT_OK = qw( entropy );
+our $VERSION = ( '$Revision: 1.2 $ ' =~ /(\d+\.\d+)/ ) ;
+our @EXPORT_OK = qw( entropy consensus);
 
 use Carp;
 
 =head2 entropy
 
 Computes the entropy using the well known Shannon's formula: L<http://en.wikipedia.org/wiki/Information_entropy>
+'to avoid botching highlighting
 
 =cut
 
@@ -52,15 +55,44 @@ sub entropy {
   return $entropy;
 }
 
+=head2 consensus
+
+Consensus sequence representing the majoritary value for each bit
+
+=cut
+
+sub consensus {
+  my $population = shift;
+  my @frequencies;
+  for ( @$population ) {
+      for ( my $i = 0; $i < $_->length(); $i ++ ) {
+	  if ( !$frequencies[$i] ) {
+	      $frequencies[$i]={ 0 => 0,
+			     1 => 0};
+	  }
+	  $frequencies[$i]->{substr($_->{'_str'}, $i, 1)}++;
+      }
+  }
+  my $consensus;
+  for my $f ( @frequencies ) {
+      if ( $f->{'0'} > $f->{'1'} ) {
+	  $consensus.='0';
+      } else {
+	  $consensus.='1';
+      }
+  }
+  return $consensus;
+}
+
 =head1 Copyright
   
   This file is released under the GPL. See the LICENSE file included in this distribution,
   or go to http://www.fsf.org/licenses/gpl.txt
 
-  CVS Info: $Date: 2008/04/03 18:37:18 $ 
-  $Header: /media/Backup/Repos/opeal/opeal/Algorithm-Evolutionary/lib/Algorithm/Evolutionary/Utils.pm,v 1.1 2008/04/03 18:37:18 jmerelo Exp $ 
+  CVS Info: $Date: 2008/04/20 11:03:12 $ 
+  $Header: /media/Backup/Repos/opeal/opeal/Algorithm-Evolutionary/lib/Algorithm/Evolutionary/Utils.pm,v 1.2 2008/04/20 11:03:12 jmerelo Exp $ 
   $Author: jmerelo $ 
-  $Revision: 1.1 $
+  $Revision: 1.2 $
   $Name $
 
 =cut
