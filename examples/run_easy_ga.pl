689842acf1770cc06772e1c6b700154126d2f4f0
jmerelo <jmerelo>
2008-10-02 05:56:55 +0000

    bug fixed

--- examples/run_easy_ga.pl
+++ examples/run_easy_ga.pl
@@ -44,17 +44,29 @@ for ( 0..$conf->{'pop_size'} ) {
   push( @pop, $indi );
 }
 
-
 #----------------------------------------------------------#
-# Variation operators
-my $m =  new Algorithm::Evolutionary::Op::Mutation( $conf->{'mutation'}->{'rate'}, $conf->{'mutation'}->{'priority'}  );
-my $c = new Algorithm::Evolutionary::Op::Crossover($conf->{'mutation'}->{'points'}, $conf->{'crossover'}->{'priority'} );
-
 # Fitness function
 my $fitness_class = "Algorithm::Evolutionary::Fitness::".$conf->{'fitness'}->{'class'};
 eval  "require $fitness_class" || die "Can't load $fitness_class: $@\n";
 my @params = $conf->{'fitness'}->{'params'}? @{$conf->{'fitness'}->{'params'}} : ();
 my $fitness_object = eval $fitness_class."->new( \@params )" || die "Can't instantiate $fitness_class: $@\n";
+
+#----------------------------------------------------------#
+# Variation operators
+my @ops;
+for my $o ( keys %{$conf->{'ops'}} ) {
+    my $op_class = "Algorithm::Evolutionary::Op::".$o;
+    eval  "require $op_class" || die "Can't load $op_class: $@\n";
+    my @params = @{$conf->{'ops'}->{$o}};
+    my $op_object = eval $op_class.'->new( @params )' || die "Can't instantiate $op_class: $@\n";
+#    my $m =  new Algorithm::Evolutionary::Op::Mutation( $conf->{'mutation'}->{'rate'}, $conf->{'mutation'}->{'priority'}  );
+#my $c = new Algorithm::Evolutionary::Op::Crossover($conf->{'crossover'}->{'points'}, $conf->{'crossover'}->{'priority'} );
+    ####################### Big hack #######################################3
+    if ( ref $op_object eq 'Algorithm::Evolutionary::Op::Novelty_Mutation' ) {
+      $op_object->{'_population_hashref'} = $fitness_object->{'_cache'};
+    }
+    push @ops, $op_object;
+}
 
 #----------------------------------------------------------#
 #Usamos estos operadores para definir una generación del algoritmo. Lo cual
@@ -63,7 +75,7 @@ my $fitness_object = eval $fitness_class."->new( \@params )" || die "Can't insta
 # operadores de variación.
 my $fitness = sub { $fitness_object->apply(@_) };
 
-my $generation = Algorithm::Evolutionary::Op::Easy->new( $fitness , $conf->{'selection_rate'} , [$m, $c] ) ;
+my $generation = Algorithm::Evolutionary::Op::Easy->new( $fitness , $conf->{'selection_rate'} , \@ops ) ;
 
 #Time
 my $inicioTiempo = [gettimeofday()];
@@ -108,10 +120,10 @@ J. J. Merelo, C<jj@merelo.net>
   This file is released under the GPL. See the LICENSE file included in this distribution,
   or go to http://www.fsf.org/licenses/gpl.txt
 
-  CVS Info: $Date: 2008/02/17 17:02:55 $ 
-  $Header: /media/Backup/Repos/opeal/opeal/Algorithm-Evolutionary/examples/run_easy_ga.pl,v 1.2 2008/02/17 17:02:55 jmerelo Exp $ 
+  CVS Info: $Date: 2008/10/02 05:56:55 $ 
+  $Header: /media/Backup/Repos/opeal/opeal/Algorithm-Evolutionary/examples/run_easy_ga.pl,v 1.3 2008/10/02 05:56:55 jmerelo Exp $ 
   $Author: jmerelo $ 
-  $Revision: 1.2 $
+  $Revision: 1.3 $
   $Name $
 
 =cut
