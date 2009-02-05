fcf44924e5d77596ec3d415e5de23b783f92d9d3
jmerelo <jmerelo>
2009-02-05 07:02:01 +0000

    Fixed uncomprehensible CPAN bugs

--- lib/Algorithm/Evolutionary/Individual/Base.pm
+++ lib/Algorithm/Evolutionary/Individual/Base.pm
@@ -34,7 +34,7 @@ use XML::Parser::EasyTree;
 use YAML qw(Dump Load LoadFile);
 use Carp;
 
-our ($VERSION) = ( '$Revision: 2.1 $ ' =~ / (\d+\.\d+)/ );
+our ($VERSION) = ( '$Revision: 2.2 $ ' =~ / (\d+\.\d+)/ );
 
 use constant MY_OPERATORS => qw(None);
 
@@ -118,7 +118,7 @@ sub fromXML {
     $xml = $p->parse($xml);
   }
 
-  my $thisClassName = $xml->[0]{attrib}{type};
+  my $thisClassName = $xml->[0]{'attrib'}{'type'};
   if ( $class eq  __PACKAGE__ ) { #Deduct class from the XML
     $class = $thisClassName || shift || croak "Class name missing";
   }
@@ -341,10 +341,10 @@ L<Algorithm::Evolutionary::Individual::Bit_Vector>
   This file is released under the GPL. See the LICENSE file included in this distribution,
   or go to http://www.fsf.org/licenses/gpl.txt
 
-  CVS Info: $Date: 2009/02/04 20:43:14 $ 
-  $Header: /media/Backup/Repos/opeal/opeal/Algorithm-Evolutionary/lib/Algorithm/Evolutionary/Individual/Base.pm,v 2.1 2009/02/04 20:43:14 jmerelo Exp $ 
+  CVS Info: $Date: 2009/02/05 07:02:01 $ 
+  $Header: /media/Backup/Repos/opeal/opeal/Algorithm-Evolutionary/lib/Algorithm/Evolutionary/Individual/Base.pm,v 2.2 2009/02/05 07:02:01 jmerelo Exp $ 
   $Author: jmerelo $ 
-  $Revision: 2.1 $
+  $Revision: 2.2 $
   $Name $
 
 =cut
