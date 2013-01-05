a944016a17cc835e099c962d774939f3ebd31c86
jmerelo <jmerelo>
2013-01-05 12:43:32 +0000

    Eliminated a couple of slow sort and substituted by SFANDINO's Sort::Key

--- lib/Algorithm/Evolutionary/Op/Replace_Different.pm
+++ lib/Algorithm/Evolutionary/Op/Replace_Different.pm
@@ -29,11 +29,13 @@ into the new population .
 
 package Algorithm::Evolutionary::Op::Replace_Different;
 
-our $VERSION =   sprintf "%d.%d", q$Revision: 1.2 $ =~ /(\d+)\.(\d+)/g; 
+our $VERSION =   sprintf "%d.%d", q$Revision: 1.3 $ =~ /(\d+)\.(\d+)/g; 
 
 use Carp;
 
 use base 'Algorithm::Evolutionary::Op::Base';
+
+use Sort::Key qw(nkeysort);
 
 #Class-wide constants
 our $ARITY = 1;
@@ -53,8 +55,7 @@ sub apply ($;$){
   my $chromosome_list = shift || croak "No new population here!";
   
   #Sort
-  my @sorted_population = sort { $b->Fitness() <=> $a->Fitness(); }
-    @$population ;
+  my @sorted_population = nkeysort { $_->Fitness() } @$population ;
 
   my %population_hash;
   map( $population_hash{$_->{'_str'}} = 1, @sorted_population );
@@ -63,7 +64,7 @@ sub apply ($;$){
   my $to_eliminate = scalar @non_repeated;
   if ( $to_eliminate > 0 ) {
     # print "Eliminating $to_eliminate\n";
-    splice ( @sorted_population, -$to_eliminate );
+    splice ( @sorted_population, 0,  $to_eliminate );
     push @sorted_population, @non_repeated;
   }
   return \@sorted_population;
@@ -85,10 +86,10 @@ Another breeder is L<Algorithm::Evolutionary::Op::Replace_Worst>.
   This file is released under the GPL. See the LICENSE file included in this distribution,
   or go to http://www.fsf.org/licenses/gpl.txt
 
-  CVS Info: $Date: 2013/01/05 12:01:58 $ 
-  $Header: /media/Backup/Repos/opeal/opeal/Algorithm-Evolutionary/lib/Algorithm/Evolutionary/Op/Replace_Different.pm,v 1.2 2013/01/05 12:01:58 jmerelo Exp $ 
+  CVS Info: $Date: 2013/01/05 12:43:32 $ 
+  $Header: /media/Backup/Repos/opeal/opeal/Algorithm-Evolutionary/lib/Algorithm/Evolutionary/Op/Replace_Different.pm,v 1.3 2013/01/05 12:43:32 jmerelo Exp $ 
   $Author: jmerelo $ 
-  $Revision: 1.2 $
+  $Revision: 1.3 $
   $Name $
 
 =cut
