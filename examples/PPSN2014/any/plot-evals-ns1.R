419250b17ab3c93874e012ff70cc8c371bc3b3bd
JJ Merelo <jjmerelo@gmail.com>
2014-03-30 19:11:38 +0200

    Should have moved this

---
+++ examples/PPSN2014/any/plot-evals-ns1.R
@@ -0,0 +1,3 @@
+boxplot(res.af.Trap.p1024.evals, res.afn.p1024.ns1.evals, res.afnm.Trap.p1024.ns1.evals, res.afnmw.p1024.ns1.evals,
+        ylab='Number', xaxt='n', main='Number of evaluations, noise sigma = 1',sub='4-Trap * 10')
+axis(1, at=c(1,2,3,4),  las=0, labels=c('base','Memory','ETA','WPO'))
